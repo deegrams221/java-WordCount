dcdc681fc5f1bc01b4b20a2ecf8972d04e6aefbe
Diana Grams <deegrams221@gmail.com>
2019-10-01 13:31:25 -0600

    set up getters and setters

--- wordcount/src/wordcount/Wordcount.java
+++ wordcount/src/wordcount/Wordcount.java
@@ -3,16 +3,31 @@ package workcount;
 public class Wordcount
 {
     // fields
+    private String words;
 
-    
     // constructor
-
-
-    // methods
-
+    public Words(String words)
+    {
+        this.words = words;
+    }
 
     // getters and setters
+    public String getWords()
+    {
+        return words;
+    }
 
+    public void setWords(String words)
+    {
+        this.words = words;
+    }
 
     // override - toString
+    @Override
+    public String toString()
+    {
+        return "Words{" +
+                "words='" + words + 
+                '}';
+    }
 }
