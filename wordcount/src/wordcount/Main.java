25a8d7a4253421c7e0ddbb6aef6d737b8ccffbef
Diana Grams <deegrams221@gmail.com>
2019-10-01 12:16:33 -0600

    initial project set up - created Main.java and Wordcount.java files

---
+++ wordcount/src/wordcount/Main.java
@@ -0,0 +1,11 @@
+package workcount;
+
+import java.util.*;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+    }   
+}
---
+++ wordcount/src/wordcount/Wordcount.java
@@ -0,0 +1,18 @@
+package workcount;
+
+public class Wordcount
+{
+    // fields
+
+    
+    // constructor
+
+
+    // methods
+
+
+    // getters and setters
+
+
+    // override - toString
+}
